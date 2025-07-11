1b728c2fbc5f4f284f01aeb0112e8f6c5bd94a85
BestPickyReviews <klsamazingfacts@gmail.com>
2025-07-11 23:47:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -38,7 +38,7 @@ FSUB_PICS = (environ.get('FSUB_PICS', 'https://graph.org/file/7478ff3eac37f4329c
 # ============================
 # Admin, Channels & Users Configuration
 # ============================
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()] # Replace with the actual admin ID(s) to add
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5207748612').split()] # Replace with the actual admin ID(s) to add
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002832129962').split()]  # Channel id for auto indexing (make sure bot is admin)
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002017490016'))  # Log channel id (make sure bot is admin)
 BIN_CHANNEL = int(environ.get('BIN_CHANNEL', '-1002017490016'))  # Bin channel id (make sure bot is admin)
@@ -97,7 +97,7 @@ GRP_LNK = environ.get('GRP_LNK', 'https://t.me/hd99flixgroup')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/hd99flix')
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/hd99flixadmincontact_Bot')
 DREAMCINEZONE_MOVIE_UPDATE_CHANNEL_LNK = environ.get('DREAMCINEZONE_MOVIE_UPDATE_CHANNEL_LNK', 'https://t.me/hdsxmedia')
-OWNERID = int(os.environ.get('OWNERID', '1234567890'))  # Replace with the actual admin ID
+OWNERID = int(os.environ.get('OWNERID', '5207748612'))  # Replace with the actual admin ID
 
 # ============================
 # User Configuration
